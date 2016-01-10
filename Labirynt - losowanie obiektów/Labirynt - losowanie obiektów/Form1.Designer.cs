485c10e4abefffda069656a260c065a76b519d01
RayRed93 <piotr.redmerski@gmail.com>
2016-01-10 16:03:12 +0100

    First Push

---
+++ Labirynt - losowanie obiektów/Labirynt - losowanie obiektów/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Labirynt___losowanie_obiektów
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+			this.groupBox4 = new System.Windows.Forms.GroupBox();
+			this.groupBox5 = new System.Windows.Forms.GroupBox();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+			this.groupBox3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+			this.groupBox5.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(386, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(171, 38);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "LOSUJ";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(7, 19);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(67, 20);
+			this.numericUpDown1.TabIndex = 1;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.numericUpDown1);
+			this.groupBox1.Location = new System.Drawing.Point(386, 56);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(80, 51);
+			this.groupBox1.TabIndex = 2;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Rozmiar X";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.numericUpDown2);
+			this.groupBox2.Location = new System.Drawing.Point(478, 56);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(80, 51);
+			this.groupBox2.TabIndex = 3;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Rozmiar Y";
+			// 
+			// numericUpDown2
+			// 
+			this.numericUpDown2.Location = new System.Drawing.Point(6, 19);
+			this.numericUpDown2.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Name = "numericUpDown2";
+			this.numericUpDown2.Size = new System.Drawing.Size(68, 20);
+			this.numericUpDown2.TabIndex = 1;
+			this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.numericUpDown3);
+			this.groupBox3.Location = new System.Drawing.Point(386, 113);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(172, 51);
+			this.groupBox3.TabIndex = 3;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Minimalan odległość";
+			// 
+			// numericUpDown3
+			// 
+			this.numericUpDown3.Location = new System.Drawing.Point(48, 19);
+			this.numericUpDown3.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+			this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown3.Name = "numericUpDown3";
+			this.numericUpDown3.Size = new System.Drawing.Size(77, 20);
+			this.numericUpDown3.TabIndex = 1;
+			this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// groupBox4
+			// 
+			this.groupBox4.Location = new System.Drawing.Point(12, 8);
+			this.groupBox4.Name = "groupBox4";
+			this.groupBox4.Size = new System.Drawing.Size(368, 368);
+			this.groupBox4.TabIndex = 4;
+			this.groupBox4.TabStop = false;
+			this.groupBox4.Text = "Labirynt";
+			this.groupBox4.Paint += new System.Windows.Forms.PaintEventHandler(this.groupBox4_Paint);
+			// 
+			// groupBox5
+			// 
+			this.groupBox5.Controls.Add(this.richTextBox1);
+			this.groupBox5.Location = new System.Drawing.Point(386, 170);
+			this.groupBox5.Name = "groupBox5";
+			this.groupBox5.Size = new System.Drawing.Size(172, 206);
+			this.groupBox5.TabIndex = 5;
+			this.groupBox5.TabStop = false;
+			this.groupBox5.Text = "Lokacje obiektów";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+			this.richTextBox1.Location = new System.Drawing.Point(7, 19);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(159, 180);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(569, 388);
+			this.Controls.Add(this.groupBox5);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.groupBox4);
+			this.DoubleBuffered = true;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Labirynty - losowanie obiektów v1.02 by Red";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+			this.groupBox3.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+			this.groupBox5.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.NumericUpDown numericUpDown2;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.NumericUpDown numericUpDown3;
+		private System.Windows.Forms.GroupBox groupBox4;
+		private System.Windows.Forms.GroupBox groupBox5;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+	}
+}
+
---
+++ Labirynt - losowanie obiektów/Labirynt - losowanie obiektów/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace Labirynt___losowanie_obiektów
+{
+	public partial class Form1 : Form
+	{
+
+		int[,] labiryntTab;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void groupBox4_Paint(object sender, PaintEventArgs e)
+		{
+			
+			if (labiryntTab != null)
+			{
+				int rectSize = 11, spacing = 10;
+				SolidBrush brush = new SolidBrush(Color.Red);
+				Pen pen = new Pen(Color.DarkBlue);
+				rectSize = (groupBox4.Size.Width-40) / labiryntTab.GetLength(0);
+				spacing = rectSize-1;
+				
+
+				for (int i = 0; i < labiryntTab.GetLength(1); i++)
+				{
+					for (int j = 0; j < labiryntTab.GetLength(0); j++)
+					{
+						Rectangle kwadrat = new Rectangle(j * rectSize + 20, i * rectSize + 20, spacing - 1, spacing - 1);
+						e.Graphics.DrawRectangle(pen, kwadrat);
+						if (labiryntTab[j, i] == 10)
+						{
+							Rectangle kolo = new Rectangle(j * rectSize + 20 + rectSize/10, i * rectSize + 20 + rectSize/10, spacing-rectSize/5, spacing-rectSize/5);
+							e.Graphics.FillEllipse(brush, kolo);
+							
+						  
+						}
+		
+					}
+				}
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			labiryntTab = new int[(int)numericUpDown1.Value,(int)numericUpDown2.Value];
+			richTextBox1.Clear();
+			int i = 1;
+			
+			foreach (var item in LosujObiekty())
+			{
+				richTextBox1.Text += "Obiekt " + i + " = " + item.ToString() + "\n";
+				i++;
+			}
+			groupBox4.Refresh();
+		}
+
+		List<Point> LosujObiekty()
+		{
+			Random rnd = new Random();
+			Point obiekt = new Point();
+			List<Point> listaObiektow = new List<Point>();
+			bool zaBlisko=false;
+			bool zaDlugo=false;
+			int licznik = 0;
+
+			while(zaDlugo==false)
+			{
+	
+				licznik = 0;
+				do
+				{
+					licznik++;
+					if (licznik > 500) 
+					{
+						zaDlugo=true;
+						break;
+					}
+					
+					obiekt.X = rnd.Next(labiryntTab.GetLength(0));
+					obiekt.Y = rnd.Next(labiryntTab.GetLength(1));
+					Debug.WriteLine(licznik);
+					zaBlisko = false;
+
+					for (int i = 0; i < labiryntTab.GetLength(1); i++)
+					{
+						for (int j = 0; j < labiryntTab.GetLength(0); j++)
+						{
+							if ((Math.Pow((j - obiekt.X), 2) + Math.Pow((i - obiekt.Y), 2) < Math.Pow((int)numericUpDown3.Value, 2)) && labiryntTab[j, i] == 10)
+							{
+								zaBlisko = true;
+								break;
+							}	
+						}
+					}
+				} while (zaBlisko == true);
+
+				if(zaDlugo==false)
+				{
+				listaObiektow.Add(obiekt);
+				labiryntTab[obiekt.X, obiekt.Y] = 10;
+				}
+			}
+			return listaObiektow;
+		}
+	}
+}
